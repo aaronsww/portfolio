57178f1ecd0c93c06150237d60f22c1fcc7a2c31
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2023-05-04 13:07:28 +0530

    refactor: used semantic html tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,46 @@
 import * as React from "react";
-import "./styles.css"
+import "../styles.css";
 
 export default function Home() {
   return (
     <div>
-      <nav>
-        <a href="">Portfolio</a>
-        <a href="">About</a>
-        <a href="">Contact</a>
-      </nav>
-      <h1>Jeevan Aaron</h1>
-      <h1>Web Developer</h1>
-      <section>
-        <h1>Projects</h1>
-      </section>
+      <header>
+        <nav>
+          <a href="">Portfolio</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </nav>
+      </header>
 
-      <secion>
-        <h1> About</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-        {/* Skill List */}
-      </secion>
+      <main>
+        <h1>Jeevan Aaron</h1>
+        <h1>Web Developer</h1>
+
+        <section>
+          <h1>Projects</h1>
+        </section>
+
+        <secion>
+          <h1> About</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          {/* Skill List */}
+        </secion>
+      </main>
       {/* Call Github Api */}
-      <secion>
+      <footer>
         <h1> Contact</h1>
         <h2>Get in touch</h2>
-      </secion>
+      </footer>
     </div>
   );
 }
