94926119ba5eb060a8e74266874fd9e5f15656bc
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2023-05-11 13:28:35 +0530

    chore: gatsby font packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,15 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900`,
+          `Sora:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800`
+        ],
+        display: 'swap'
+      },
+    },
   ],
 };
